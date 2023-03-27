1642c2e25f0fdd9cc540054dd9c885fdb580d467
Piyush <piyushkumarsnd@gmail.com>
2023-03-27 11:30:56 +0530

    data persisted using json server

--- src/App.js
+++ src/App.js
@@ -1,36 +1,56 @@
-import React,{useState} from "react";
-import BookCreate from './components/BookCreate.js';
-import BookList from './components/BookList.js'
+import React, { useState, useEffect } from "react";
+import BookCreate from "./components/BookCreate.js";
+import BookList from "./components/BookList.js";
+import axios from "axios";
 
 export default function App() {
-  const [books,setBooks] = useState([]);
-  const deleteBookById = (id)=>{
-    setBooks(books.filter((book)=>{
-      return book.id !==id;
-    }))
-  
-  }
-  const editBookById =(id,title)=>{
-    const updatedBooks = books.map((book,index)=>{
-      if(book.id===id) return {...book,title}
+  const [books, setBooks] = useState([]);
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
+    setBooks(
+      books.filter((book) => {
+        return book.id !== id;
+      })
+    );
+  };
+  const editBookById = async (id, title) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title,
+    });
+    console.log(response);
+    const updatedBooks = books.map((book, index) => {
+      if (book.id === id) return { ...book, ...response.data };
       return book;
-    })
+    });
     setBooks(updatedBooks);
-  }
-  const createBook =(title)=>{
-      console.log('to add book with title',title)
-      setBooks([...books,
-      { id: Math.round(Math.random()*9999),
-        title
-      }
-    ])
-    console.log(books)
-  }
+  };
+
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    console.log(response);
+    setBooks(response.data);
+  };
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const createBook = async (title) => {
+    console.log("to add book with title", title);
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+    console.log(response);
+
+    setBooks([...books, response]);
+    console.log(books);
+  };
+
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <BookList onDelete ={deleteBookById} onEdit ={editBookById} books ={books}/>
-     <BookCreate  onCreate={createBook}/>
+      <BookList onDelete={deleteBookById} onEdit={editBookById} books={books} />
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
